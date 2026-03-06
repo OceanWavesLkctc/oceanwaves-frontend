eaf51a4ebab4bec8a94cb84f4202624b867e6c4e
Mir-Danish <mirdilber43@gmail.com>
2026-03-06 15:35:24 +0530

    changes on the student dashboard screen

--- src/screens/student/StudentDashboard.js
+++ src/screens/student/StudentDashboard.js
@@ -1,17 +1,38 @@
-import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
+import { View, Text, StyleSheet, TextInput, ScrollView, Image, Platform, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar, Card, IconButton } from 'react-native-paper';
 
 export default function StudentDashboard() {
     return (
+        <SafeAreaView style={{flex:1}}>
         <ScrollView style={styles.container}>
+            <View style={{flex:1,justifyContent:"center",alignItems:"center",marginBottom:"20"}}>
+            
+            <Image source={require("./images/logo.png")} style={{height:"100",width:"100", borderRadius: 75, resizeMode:"contain"}}/>
+            <Text style={styles.header}>Ocean Waves</Text>
+            <Text>Resource Sharing for Academics</Text>
+            </View>
 
-            <Text style={styles.header}>Welcome Back</Text>
+            <View style={{flex:1, justifyContent:"center",alignItems:"center",marginBottom:"10",width:"100%"}}>
+                <Image source={require("./images/group.png")} style={{height:"180",borderRadius:75, resizeMode:"contain"}} />
+            </View>
 
             <TextInput
                 placeholder="Search subjects or resources..."
                 style={styles.search}
             />
 
-            <Text style={styles.section}>Recent Uploads</Text>
+
+            <Text style={styles.section}>Preview Resources</Text>
+
+            <View style={{flexDirection:"row",justifyContent:"space-evenly",alignItems:"center", marginHorizontal:"20"}}> 
+                <View style={{borderWidth:1,width:150, height:130,borderRadius:10,padding:4}}>
+                    <Text style={{fontWeight:"bold",alignSelf:"center"}}>Computer Science</Text>
+                </View>
+                <View style={{borderWidth:1,width:150, height:130,borderRadius:10,padding:4}}>
+                    <Text style={{fontWeight:"bold",alignSelf:"center"}}>Literature</Text>
+                </View>
+            </View>
 
             <View style={styles.card}>
                 <Text>Operating Systems Notes</Text>
@@ -31,19 +52,20 @@ export default function StudentDashboard() {
             </View>
 
         </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24,
+        //paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         backgroundColor: "#F6F5FB"
     },
     header: {
         fontSize: 26,
+        fontFamily:"Poppins",
         fontWeight: "bold",
-        marginBottom: 16
     },
     search: {
         backgroundColor: "#fff",
